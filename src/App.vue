264241219d1bd2cf52b71201611432d103a8fede
sder12 <sara.cetto@gmail.com>
2023-01-19 16:22:07 +0100

    Scaffolding with sass-bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+
+  <div class="container">
+    <div class="row justify-content-center">
+      <h1 class="text-center">AppVue</h1>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use "./styles/general.scss" as *;
 </style>
