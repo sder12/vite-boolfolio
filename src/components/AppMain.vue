20540a23874b01edb1f8713f3dd32a88a9b556a8
sder12 <sara.cetto@gmail.com>
2023-01-19 16:55:55 +0100

    Axios request, saving the data in array

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,34 @@
 <script>
+import axios from 'axios';
+
 export default {
-    name: "AppMain"
+    name: "AppMain",
+    data() {
+        return {
+            apiUrl: "http://127.0.0.1:8000/api/projects",
+            codeUrl: "http://127.0.0.1:8000",
+            projects: []
+        }
+    },
+    created() {
+        axios.get(this.apiUrl).then((resp) =>
+            this.projects = resp.data.results
+        )
+    }
+
 }
+
+
 </script>
 
 <template>
     <main class="bg-primary">
         <h2>Main</h2>
+        <ul>
+            <li v-for="project in projects">
+                {{ project.title }}
+            </li>
+        </ul>
     </main>
 </template>
 
