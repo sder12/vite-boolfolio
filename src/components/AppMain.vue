c7555909550141763767fd2e722d5c259e49e7f5
sder12 <sara.cetto@gmail.com>
2023-01-19 17:24:29 +0100

    Working on the layout of the vards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -22,13 +22,34 @@ export default {
 </script>
 
 <template>
-    <main class="bg-primary">
-        <h2>Main</h2>
-        <ul>
-            <li v-for="project in projects">
-                {{ project.title }}
-            </li>
-        </ul>
+    <main class="container bg-light py-3">
+        <h4 class="text-center fw-bold mb-4">PROJECTS</h4>
+
+        <div class="row justify-content-center">
+            <!-- Col + Card -->
+            <div class="col-11 col-md-6 col-lg-4 d-flex justify-content-center mt-3" v-for="project in projects"
+                :key="project.id">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ project.title }}</h5>
+                        <p class="card-text">
+                            Description
+                        </p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Technology</li>
+                        <li class="list-group-item">Type</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Show</a>
+                    </div>
+                </div>
+            </div>
+            <!-- / Col + Card -->
+
+
+        </div>
     </main>
 </template>
 
