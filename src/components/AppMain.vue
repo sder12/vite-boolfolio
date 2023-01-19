121b3326f0de37717bf80f7b43cdffa179c9bf97
sder12 <sara.cetto@gmail.com>
2023-01-19 17:29:34 +0100

    Create new Card component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,21 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 
 export default {
     name: "AppMain",
+    components: { ProjectCard },
     data() {
         return {
             apiUrl: "http://127.0.0.1:8000/api/projects",
             codeUrl: "http://127.0.0.1:8000",
             projects: []
-        }
+        };
     },
     created() {
-        axios.get(this.apiUrl).then((resp) =>
-            this.projects = resp.data.results
-        )
-    }
-
+        axios.get(this.apiUrl).then((resp) => this.projects = resp.data.results);
+    },
+    components: { ProjectCard }
 }
 
 
@@ -26,8 +26,12 @@ export default {
         <h4 class="text-center fw-bold mb-4">PROJECTS</h4>
 
         <div class="row justify-content-center">
+
+            <ProjectCard v-for="project in projects" :key="project.id" />
+
+
             <!-- Col + Card -->
-            <div class="col-11 col-md-6 col-lg-4 d-flex justify-content-center mt-3" v-for="project in projects"
+            <!-- <div class="col-11 col-md-6 col-lg-4 d-flex justify-content-center mt-3" v-for="project in projects"
                 :key="project.id">
                 <div class="card" style="width: 18rem;">
                     <img src="..." class="card-img-top" alt="...">
@@ -45,10 +49,8 @@ export default {
                         <a href="#" class="card-link">Show</a>
                     </div>
                 </div>
-            </div>
+            </div> -->
             <!-- / Col + Card -->
-
-
         </div>
     </main>
 </template>
