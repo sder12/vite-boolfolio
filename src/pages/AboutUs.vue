07f8d2d5877a22868538e7d03a2e057c115c2b9a
sder12 <sara.cetto@gmail.com>
2023-01-20 15:59:53 +0100

    Renaming pages + adding components to router.js

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-    name: "AppAbout"
+    name: "AboutUs"
 }
 </script>
 
--- src/router.js
+++ src/router.js
@@ -1,9 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import HomePage from './pages/HomePage.vue';
+import AboutUs from './pages/AboutUs.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage,
+        },
+        {
+            path: '/about-us',
+            name: 'about',
+            component: AboutUs,
+        }
     ],
 });
 
