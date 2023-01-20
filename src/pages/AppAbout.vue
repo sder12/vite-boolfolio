7f0b8f7f9fc644f8933b5c7d1cd6a7c695828f7e
sder12 <sara.cetto@gmail.com>
2023-01-20 15:50:12 +0100

    Install router and create two components in pages

---
+++ src/pages/AppAbout.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "AppAbout"
+}
+</script>
+
+<template>
+    <h2>App About</h2>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/AppHome.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "AppHome",
+}
+</script>
+
+<template>
+    <h2>App Home</h2>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
