79dcd8d4227319126298eefeced0575f3dd5d118
sder12 <sara.cetto@gmail.com>
2023-01-23 16:48:15 +0100

    Check if the slug corrisponds to obj else 404

--- src/pages/SingleProj.vue
+++ src/pages/SingleProj.vue
@@ -17,8 +17,14 @@ export default {
             // console.log(this.$route.params.slug, this.store.apiUrl)
             axios.get(`${this.store.apiUrl}/api/projects/${this.$route.params.slug}`)
                 .then(resp => {
-                    this.project = resp.data.project;
-                    console.log('project', this.project.title);
+                    // console.log(resp.data.success);
+                    // true quando slug esiste false quando non c'è
+                    if (resp.data.success) {
+                        this.project = resp.data.project;
+                    } else {
+                        //rindirizza alla pagina 404
+                        this.$router.push({ name: "not-found" });
+                    };
                 })
         }
     }
