3dc062e0013d4291fc305f31b52c582c07ca4594
sder12 <sara.cetto@gmail.com>
2023-01-23 16:35:14 +0100

    Create debug method to check the $route.params

--- src/pages/SingleProj.vue
+++ src/pages/SingleProj.vue
@@ -1,6 +1,20 @@
 <script>
+import { store } from '../store';
 export default {
     name: 'SingleProj',
+    data() {
+        return {
+            store
+        }
+    },
+    created() {
+        this.getSinglePoject();
+    },
+    methods: {
+        getSinglePoject() {
+            console.log(this.$route.params.slug, this.store.apiUrl)
+        }
+    }
 }
 </script>
 
